e783524bb7d485b08a8ca248a32b63df90290777
Jeffrey Abraham <Jeffreyabraham27@gmail.com>
2022-11-18 18:40:59 -0500

    Remove Bank class from User folder

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "User/Member.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    Member member("John Doe", "JohnDoe@gmail.com");
+    Member member2("John Doe", "JohnDoe@gmail.com");
+
+    member.updateCheckingAccountBalance(1000);
+    std::cout << member.showCheckingAccountBalance() << '\n';
+
     return 0;
 }
