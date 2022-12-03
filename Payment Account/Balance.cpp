cce17aae8889a1006299c80d4d2da2b9322c1e84
Jeffrey Abraham <Jeffreyabraham27@gmail.com>
2022-12-03 09:19:37 -0500

    Added a Bank directory

---
+++ Payment Account/Balance.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jeffrey Abraham on 12/3/22.
+//
+
+#include "Balance.h"
---
+++ Payment Account/Balance.h
@@ -0,0 +1,14 @@
+//
+// Created by Jeffrey Abraham on 12/3/22.
+//
+
+#ifndef ATM_BALANCE_H
+#define ATM_BALANCE_H
+
+
+class Balance {
+
+};
+
+
+#endif //ATM_BALANCE_H
